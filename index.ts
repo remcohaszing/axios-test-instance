c00c1355e8ac1a61faf24a75da94bc6e87e399ea
Remco Haszing <remcohaszing@gmail.com>
2022-08-15 10:32:07 +0200

    Define Application in types.d.ts
    This prevents TypeScript from stripping the `@ts-ignore` comments.

--- index.ts
+++ index.ts
@@ -1,16 +1,10 @@
-import { createServer, RequestListener } from 'http';
+import { createServer } from 'http';
 import { AddressInfo } from 'net';
 
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
-// @ts-ignore
-import { FastifyInstance } from 'fastify';
-// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
-// @ts-ignore
-import * as Koa from 'koa';
 
-type FastifyLike = unknown extends FastifyInstance ? never : FastifyInstance;
-type KoaLike = unknown extends Koa ? never : Koa;
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { type Application } from './types.js';
 
 /**
  * An Axios instance that is bound to a test server.
@@ -34,11 +28,7 @@ interface RunningServer {
   close: () => Promise<void>;
 }
 
-/**
- * A web server application that represents either an HTTP callback function or a Koa or Fastify
- * instance.
- */
-export type Application = FastifyLike | KoaLike | RequestListener;
+export { Application };
 
 /**
  * Assign form-data headers to the axios request config.
---
+++ types.d.ts
@@ -0,0 +1,17 @@
+import { RequestListener } from 'http';
+
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
+import { FastifyInstance } from 'fastify';
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
+import * as Koa from 'koa';
+
+type FastifyLike = unknown extends FastifyInstance ? never : FastifyInstance;
+type KoaLike = unknown extends Koa ? never : Koa;
+
+/**
+ * A web server application that represents either an HTTP callback function or a Koa or Fastify
+ * instance.
+ */
+export type Application = FastifyLike | KoaLike | RequestListener;
